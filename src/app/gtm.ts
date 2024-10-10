b2f510b91aa18608ac8b5aa45d4746720eec2a85
Ayesha-chaudhry <ashijutt069@gmail.com>
2024-10-11 03:03:31 +0500

    add gtm

---
+++ src/app/gtm.ts
@@ -0,0 +1,9 @@
+// (window as any)?.dataLayer = window?.dataLayer || [];
+function gtag() {
+    ((window as any)?.dataLayer || []).push(arguments)
+}
+// @ts-ignore
+gtag('js', new Date());
+
+// @ts-ignore
+gtag('config', 'G-G0RQJVC196');
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../../theme/theme";
+import GoogleTagManager from "@/utils/gtm";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <GoogleTagManager containerId={process.env.NEXT_PUBLIC_GTM_ID} />
+        )}
+      </head>
+
       <body className={inter.className}>
+      <noscript> <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6T49V59"></iframe></noscript>
         {" "}
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
