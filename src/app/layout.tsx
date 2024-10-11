456cbb67c2463966abb0bade49b7de7b8ddb60a0
Ayesha-chaudhry <ashijutt069@gmail.com>
2024-10-11 23:44:03 +0500

    change script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -10,17 +10,14 @@ import Leadfeeder from "@/utils/leadfeeder";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   useEffect(() => {
-    const leadfeederScript = document.createElement('script');
-    leadfeederScript.innerHTML = `
+    const script = document.createElement("script");
+    script.innerHTML = `
       (function(l,e,a,d,f,eeder){
         l['LeadfeederObject']=f;l[f]=l[f]||function(){
         (l[f].q=l[f].q||[]).push(arguments)},l[f].l=1*new Date();
@@ -28,43 +25,40 @@ export default function RootLayout({
         e.async=1;e.src='https://lftracker.leadfeeder.com/lftracker_v1.js';
         f.parentNode.insertBefore(e,f)
       }(window,document,'script','script','lf'));
+
       lf('init', '${process.env.NEXT_PUBLIC_LEADFEEDER_ID}');
     `;
-    document.head.appendChild(leadfeederScript);
+    document.head.appendChild(script);
   }, []);
-  
-// useEffect(() => {
-//   // Add Leadfeeder script within useEffect (optional for potential async execution)
-//   const script = document.createElement('script');
-//   script.async = true;
-//   script.src = 'https://sc.lfeeder.com/lftracker_v1_bElvO73oMG67ZMqj.js'; // Assuming your site ID is 'bElvO730vrb4ZMqj'
-//   document.head.appendChild(script);
-// }, [])
 
+  // useEffect(() => {
+  //   // Add Leadfeeder script within useEffect (optional for potential async execution)
+  //   const script = document.createElement('script');
+  //   script.async = true;
+  //   script.src = 'https://sc.lfeeder.com/lftracker_v1_bElvO73oMG67ZMqj.js'; // Assuming your site ID is 'bElvO730vrb4ZMqj'
+  //   document.head.appendChild(script);
+  // }, [])
 
   return (
     <html lang="en">
       <head>
         {process.env.NEXT_PUBLIC_GTM_ID && (
           <GoogleTagManager containerId={process.env.NEXT_PUBLIC_GTM_ID} />
-        )},
+        )}
+        ,
         {/* {process.env.NEXT_PUBLIC_LEADFEEDER_ID && (
           <Leadfeeder/>
         )} */}
-
-        <head>
-
-</head>
-
       </head>
 
       <body className={inter.className}>
-      <noscript>
-  <iframe src="https://sc.lfeeder.com/lftracker_v1.js"></iframe>
-</noscript>
-
-      <noscript> <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6T49V59"></iframe></noscript>
-        {" "}
+        <noscript>
+          <iframe src="https://sc.lfeeder.com/lftracker_v1.js"></iframe>
+        </noscript>
+        <noscript>
+          {" "}
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6T49V59"></iframe>
+        </noscript>{" "}
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
     </html>
